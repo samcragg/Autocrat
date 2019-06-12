b44bd4dc88036b5916500af3b47bb9b13accd200
Samuel Cragg <sam.cragg@outlook.com>
2019-06-12 07:15:51 +0100

    Add support for mapping interfaces to classes

---
+++ src/Autocrat.Compiler/InterfaceResolver.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Allows the matching of concrete types to an interface.
+    /// </summary>
+    internal class InterfaceResolver
+    {
+        private readonly Dictionary<Type, List<Type>> interfaceClasses =
+            new Dictionary<Type, List<Type>>();
+
+        /// <summary>
+        /// Adds the specified class types as candidates when resolving
+        /// interface types.
+        /// </summary>
+        /// <param name="classes">The collection of classes to add.</param>
+        public virtual void AddKnownClasses(IEnumerable<Type> classes)
+        {
+            foreach (Type classType in classes.Where(c => c.IsClass && !c.IsAbstract))
+            {
+                foreach (Type interfaceType in classType.GetInterfaces())
+                {
+                    if (!this.interfaceClasses.TryGetValue(interfaceType, out List<Type> mappedClasses))
+                    {
+                        mappedClasses = new List<Type>();
+                        this.interfaceClasses.Add(interfaceType, mappedClasses);
+                    }
+
+                    mappedClasses.Add(classType);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds the classes that implement the specified interface.
+        /// </summary>
+        /// <param name="interfaceType">The type of the interface.</param>
+        /// <returns>A list of all classes that implement the specified type.</returns>
+        public virtual IReadOnlyList<Type> FindClasses(Type interfaceType)
+        {
+            if (this.interfaceClasses.TryGetValue(interfaceType, out List<Type> mappedClasses))
+            {
+                return mappedClasses;
+            }
+            else
+            {
+                return Array.Empty<Type>();
+            }
+        }
+    }
+}
---
+++ tests/Compiler.Tests/InterfaceResolverTests.cs
@@ -0,0 +1,84 @@
+﻿namespace Compiler.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Xunit;
+
+    public class InterfaceResolverTests
+    {
+        private readonly InterfaceResolver resolver = new InterfaceResolver();
+
+        private interface IFakeInterface
+        {
+        }
+
+        public class FindClassesTests : InterfaceResolverTests
+        {
+            [Fact]
+            public void ShouldNotReturnAbstractClasses()
+            {
+                this.resolver.AddKnownClasses(new[] { typeof(AbstractClass) });
+
+                IReadOnlyList<Type> result = this.resolver.FindClasses(
+                    typeof(IFakeInterface));
+
+                result.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReturnAllClassesImplementingAnInterface()
+            {
+                Type[] classes = new[]
+                {
+                    typeof(FakeClass1),
+                    typeof(FakeClass2)
+                };
+
+                this.resolver.AddKnownClasses(classes);
+
+                IReadOnlyList<Type> result = this.resolver.FindClasses(
+                    typeof(IFakeInterface));
+
+                result.Should().BeEquivalentTo(classes);
+            }
+
+            [Fact]
+            public void ShouldReturnAnEmptyListForUnknownInterfaces()
+            {
+                IReadOnlyList<Type> result = this.resolver.FindClasses(
+                    typeof(IFakeInterface));
+
+                result.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReturnClassesForBaseInterfaces()
+            {
+                this.resolver.AddKnownClasses(new[] { typeof(DerivedClass) });
+
+                IReadOnlyList<Type> result = this.resolver.FindClasses(
+                    typeof(IFakeInterface));
+
+                result.Should().ContainSingle().Which.Should().Be(typeof(DerivedClass));
+            }
+        }
+
+        private abstract class AbstractClass : IFakeInterface
+        {
+        }
+
+        private class DerivedClass : AbstractClass
+        {
+        }
+
+        private class FakeClass1 : IFakeInterface
+        {
+        }
+
+        private class FakeClass2 : IFakeInterface
+        {
+        }
+    }
+}
