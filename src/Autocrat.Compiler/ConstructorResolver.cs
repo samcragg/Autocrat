491594697ccf46f66c92cdd9e260b405b7fe9ba3
Samuel Cragg <sam.cragg@outlook.com>
2019-06-14 06:45:44 +0100

    Add support for resolving constructors

---
+++ src/Autocrat.Compiler/ConstructorResolver.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// Allows the resolving of constructor parameters for a type.
+    /// </summary>
+    internal class ConstructorResolver
+    {
+        private readonly InterfaceResolver interfaceResolver;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructorResolver"/> class.
+        /// </summary>
+        /// <param name="interfaceResolver">
+        /// Used to resolve classes implementing interfaces.
+        /// </param>
+        public ConstructorResolver(InterfaceResolver interfaceResolver)
+        {
+            this.interfaceResolver = interfaceResolver;
+        }
+
+        /// <summary>
+        /// Finds the classes for injecting into the constructor.
+        /// </summary>
+        /// <param name="classType">The type to find the constructor for.</param>
+        /// <returns>The types needed by the constructor.</returns>
+        /// <remarks>
+        /// The types returned will be in the order expected by the constructor.
+        /// </remarks>
+        public Type[] GetParameters(Type classType)
+        {
+            // Classes always have one constructor, hence the call to First
+            ParameterInfo[] constructorParameters =
+                classType.GetConstructors()
+                         .Select(c => c.GetParameters())
+                         .OrderByDescending(p => p.Length)
+                         .First();
+
+            Array.Sort(constructorParameters, (a, b) => a.Position.CompareTo(b.Position));
+            return Array.ConvertAll(constructorParameters, this.ResolveParameterType);
+        }
+
+        private static Type GetArrayDependencyType(Type type)
+        {
+            if (type.IsArray)
+            {
+                return type;
+            }
+
+            if (type.IsGenericType)
+            {
+                Type[] arguments = type.GetGenericArguments();
+                if (arguments.Length == 1)
+                {
+                    Type arrayType = arguments[0].MakeArrayType();
+                    if (type.IsAssignableFrom(arrayType))
+                    {
+                        return arrayType;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private Type ResolveParameterType(ParameterInfo parameter)
+        {
+            Type arrayType = GetArrayDependencyType(parameter.ParameterType);
+            if (arrayType != null)
+            {
+                return arrayType;
+            }
+
+            IReadOnlyCollection<Type> classes = this.interfaceResolver.FindClasses(
+                parameter.ParameterType);
+
+            switch (classes.Count)
+            {
+                case 0:
+                    throw new InvalidOperationException(
+                        "Unable to find a class for the dependency " + parameter.ParameterType.Name);
+
+                case 1:
+                    return classes.First();
+
+                default:
+                    throw new InvalidOperationException(
+                        "Multiple dependencies found for " + parameter.ParameterType.Name);
+            }
+        }
+    }
+}
---
+++ tests/Compiler.Tests/ConstructorResolverTests.cs
@@ -0,0 +1,134 @@
+﻿namespace Compiler.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using NSubstitute;
+    using Xunit;
+
+    public class ConstructorResolverTests
+    {
+        private readonly InterfaceResolver interfaceResolver;
+        private readonly ConstructorResolver resolver;
+
+        private ConstructorResolverTests()
+        {
+            this.interfaceResolver = Substitute.For<InterfaceResolver>();
+            this.interfaceResolver.FindClasses(null)
+                .ReturnsForAnyArgs(ci => new[] { ci.Arg<Type>() });
+
+            this.resolver = new ConstructorResolver(this.interfaceResolver);
+        }
+
+        public sealed class GetParametersTests : ConstructorResolverTests
+        {
+            [Fact]
+            public void ShouldReturnAnEmptyArrayForDefaultConstructors()
+            {
+                Type[] result = this.resolver.GetParameters(typeof(DefaultConstructor));
+
+                result.Should().BeEmpty();
+            }
+
+            [Fact]
+            public void ShouldReturnArrayDependencies()
+            {
+                Type[] result = this.resolver.GetParameters(typeof(ArrayOfDependencies));
+
+                result.Should().ContainSingle().Which.Should().Be(typeof(AbstractClass[]));
+                this.interfaceResolver.DidNotReceiveWithAnyArgs().FindClasses(null);
+            }
+
+            [Fact]
+            public void ShouldReturnArrayCompatibleDependencies()
+            {
+                Type[] result = this.resolver.GetParameters(typeof(MultipleDependencies));
+
+                result.Should().ContainSingle().Which.Should().Be(typeof(AbstractClass[]));
+                this.interfaceResolver.DidNotReceiveWithAnyArgs().FindClasses(null);
+            }
+
+            [Fact]
+            public void ShouldReturnDependencyTypes()
+            {
+                Type[] result = this.resolver.GetParameters(typeof(SingleDependency));
+
+                result.Should().ContainSingle().Which.Should().Be(typeof(AbstractClass));
+            }
+
+            [Fact]
+            public void ShouldReturnTheConstructorWithTheMostParameters()
+            {
+                Type[] result = this.resolver.GetParameters(typeof(MultipleConstructors));
+
+                result.Should().HaveCount(2);
+                result.Should().HaveElementAt(0, typeof(AbstractClass));
+                result.Should().HaveElementAt(1, typeof(DefaultConstructor));
+            }
+
+            [Fact]
+            public void ShouldThrowIfMultipleDependenciesAreFound()
+            {
+                this.interfaceResolver.FindClasses(typeof(AbstractClass))
+                    .Returns(new Type[2]);
+
+                this.resolver.Invoking(x => x.GetParameters(typeof(SingleDependency)))
+                    .Should().Throw<InvalidOperationException>()
+                    .WithMessage("Multiple*");
+            }
+
+            [Fact]
+            public void ShouldThrowIfNoDependenciesAreFound()
+            {
+                this.interfaceResolver.FindClasses(typeof(AbstractClass))
+                    .Returns(Array.Empty<Type>());
+
+                this.resolver.Invoking(x => x.GetParameters(typeof(SingleDependency)))
+                    .Should().Throw<InvalidOperationException>()
+                    .WithMessage("Unable to find*");
+            }
+        }
+
+#pragma warning disable IDE0060 // Remove unused parameter
+        private abstract class AbstractClass
+        {
+        }
+
+        private class ArrayOfDependencies
+        {
+            public ArrayOfDependencies(AbstractClass[] a)
+            {
+            }
+        }
+
+        private class DefaultConstructor
+        {
+        }
+
+        private class MultipleConstructors
+        {
+            public MultipleConstructors(AbstractClass a)
+            {
+            }
+
+            public MultipleConstructors(AbstractClass a, DefaultConstructor d)
+            {
+            }
+        }
+
+        private class MultipleDependencies
+        {
+            public MultipleDependencies(IEnumerable<AbstractClass> a)
+            {
+            }
+        }
+
+        private class SingleDependency
+        {
+            public SingleDependency(AbstractClass a)
+            {
+            }
+        }
+    }
+}
