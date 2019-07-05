0409fff32b50578229d641967266a8aeceeddb3c
Samuel Cragg <sam.cragg@outlook.com>
2019-07-05 07:08:19 +0100

    Allow the replacing of native adapter class methods

---
+++ src/Autocrat.Compiler/NativeRegisterRewriter.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+    /// <summary>
+    /// Rewrites calls to interfaces methods with calls to their native
+    /// implemented ones.
+    /// </summary>
+    internal class NativeRegisterRewriter : CSharpSyntaxRewriter
+    {
+        // This class transforms an expression in the form of:
+        //// interfaceInstance.Register<Class>(arguments);
+        // to:
+        //// ExportedNativeMethods.Register_Method1(arguments, "Class_Method1");
+        //// ExportedNativeMethods.Register_Method2(arguments, "Class_Method2");
+        private readonly TypeSyntax adapterType;
+        private readonly ManagedCallbackGenerator callbackGenerator;
+        private readonly string classToReplace;
+        private readonly SemanticModel model;
+
+        private readonly List<(SyntaxNode original, SyntaxNode[] replacements)> nodesToReplace =
+            new List<(SyntaxNode, SyntaxNode[])>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NativeRegisterRewriter"/> class.
+        /// </summary>
+        /// <param name="model">Contains the semantic information.</param>
+        /// <param name="callbackGenerator">
+        /// Allows the creation of callback methods.
+        /// </param>
+        /// <param name="classType">
+        /// The class type to replace the static calls from.
+        /// </param>
+        /// <param name="adapterType">
+        /// The adapter type to replace the calls to.
+        /// </param>
+        public NativeRegisterRewriter(
+            SemanticModel model,
+            ManagedCallbackGenerator callbackGenerator,
+            Type classType,
+            Type adapterType)
+        {
+            this.model = model;
+            this.callbackGenerator = callbackGenerator;
+            this.adapterType = ParseTypeName(adapterType.FullName.Replace('+', '.'));
+            this.classToReplace = classType.FullName.Replace('+', '.');
+        }
+
+        /// <inheritdoc />
+        public override SyntaxNode VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            if (node.Expression is MemberAccessExpressionSyntax member)
+            {
+                var method = (IMethodSymbol)this.model
+                    .GetMemberGroup(member.Name)
+                    .Single();
+
+                string containingType = method.ContainingType.ToDisplayString();
+                if (method.IsGenericMethod &&
+                    this.classToReplace.Equals(containingType, StringComparison.Ordinal))
+                {
+                    SyntaxNode[] replacements =
+                        this.ReplaceNode(node, method)
+                        .Select(n => ExpressionStatement(n).WithTriviaFrom(node.Parent))
+                        .ToArray();
+
+                    this.nodesToReplace.Add((node.Parent, replacements));
+                    this.ReplaceNode(node, method);
+                }
+            }
+
+            return base.VisitInvocationExpression(node);
+        }
+
+        /// <inheritdoc />
+        public override SyntaxList<TNode> VisitList<TNode>(SyntaxList<TNode> list)
+        {
+            SyntaxList<TNode> newList = base.VisitList(list);
+            for (int i = this.nodesToReplace.Count - 1; i >= 0; i--)
+            {
+                (SyntaxNode original, SyntaxNode[] replacements) = this.nodesToReplace[i];
+                if (original is TNode)
+                {
+                    newList = newList.ReplaceRange((TNode)original, replacements.Cast<TNode>());
+                    this.nodesToReplace.RemoveAt(i);
+                }
+            }
+
+            return newList;
+        }
+
+        private static IEnumerable<IMethodSymbol> GetMethodsToExport(IMethodSymbol methodInfo)
+        {
+            return methodInfo
+                .OriginalDefinition
+                .TypeParameters
+                .SelectMany(t => t.ConstraintTypes)
+                .SelectMany(c => c.GetMembers())
+                .OfType<IMethodSymbol>()
+                .Where(m => m.MethodKind == MethodKind.Ordinary);
+        }
+
+        private IEnumerable<InvocationExpressionSyntax> ReplaceNode(
+            InvocationExpressionSyntax invocation,
+            IMethodSymbol originalMethod)
+        {
+            foreach (IMethodSymbol method in GetMethodsToExport(originalMethod))
+            {
+                ArgumentSyntax memberArgument = Argument(
+                    LiteralExpression(
+                        SyntaxKind.StringLiteralExpression,
+                        Literal(this.callbackGenerator.CreateMethod(method).ToString())));
+
+                ExpressionSyntax adapterMethod = MemberAccessExpression(
+                    SyntaxKind.SimpleMemberAccessExpression,
+                    this.adapterType,
+                    IdentifierName(method.Name));
+
+                ArgumentListSyntax arguments = invocation.ArgumentList
+                    .AddArguments(memberArgument)
+                    .NormalizeWhitespace();
+
+                yield return InvocationExpression(adapterMethod)
+                    .WithArgumentList(arguments)
+                    .WithTriviaFrom(invocation);
+            }
+        }
+    }
+}
---
+++ tests/Compiler.Tests/NativeRegisterRewriterTests.cs
@@ -0,0 +1,117 @@
+﻿namespace Compiler.Tests
+{
+    using System;
+    using System.Linq;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using NSubstitute;
+    using Xunit;
+
+    public class NativeRegisterRewriterTests
+    {
+        private const string TestNativeAdapterName =
+            "Compiler.Tests." + nameof(NativeRegisterRewriterTests) + "." + nameof(TestNativeAdapter);
+
+        private readonly ManagedCallbackGenerator callbackGenerator;
+
+        private NativeRegisterRewriterTests()
+        {
+            this.callbackGenerator = Substitute.For<ManagedCallbackGenerator>();
+            this.callbackGenerator.CreateMethod(null)
+                .ReturnsForAnyArgs(ci => SyntaxFactory.ParseName("Callback_" + ci.Arg<IMethodSymbol>().Name));
+        }
+
+        public interface IMultipleMethods
+        {
+            void Method1();
+
+            void Method2();
+        }
+
+        public interface ISingleMethod
+        {
+            void InterfaceMethod();
+        }
+
+        public interface ITestInterface
+        {
+            void MultipleMethods<T>() where T : IMultipleMethods;
+
+            void SingleArgument<T>(int value) where T : ISingleMethod;
+        }
+
+        private string VisitCode(Type interfaceType, string code)
+        {
+            Compilation compilation = CompilationHelper.CompileCode(@"
+using static Compiler.Tests.NativeRegisterRewriterTests;
+class TestClass{ " + code + "}");
+
+            SyntaxTree tree = compilation.SyntaxTrees.First();
+            SemanticModel model = compilation.GetSemanticModel(tree);
+
+            var rewriter = new NativeRegisterRewriter(
+                model,
+                this.callbackGenerator,
+                interfaceType,
+                typeof(TestNativeAdapter));
+
+            return rewriter.Visit(tree.GetRoot()).ToFullString();
+        }
+
+        public class VisitTests : NativeRegisterRewriterTests
+        {
+            [Fact]
+            public void ShouldIgnoreOtherInterfacesPassedInTheMethod()
+            {
+                const string original = @"void Method(System.IDisposable instance)
+{
+    instance.Dispose();
+}";
+
+                string result = this.VisitCode(typeof(ICloneable), original);
+
+                result.Should().Contain(original);
+            }
+
+            [Fact]
+            public void ShouldIncludeTheOriginalArgumentsWhenReplacingTheCall()
+            {
+                const string original = @"void Method(ITestInterface instance)
+{
+    int other = 123;
+    instance.SingleArgument<TestClass>(other);
+}";
+                string result = this.VisitCode(typeof(ITestInterface), original);
+
+                result.Should().Contain(@"void Method(ITestInterface instance)
+{
+    int other = 123;
+    " + TestNativeAdapterName + @".InterfaceMethod(other, ""Callback_InterfaceMethod"");
+}");
+            }
+
+            [Fact]
+            public void ShouldReplaceAllCallsOnTheInterface()
+            {
+                const string original = @"void Method(ITestInterface instance)
+{
+    instance.MultipleMethods<TestClass>();
+}";
+
+                string result = this.VisitCode(typeof(ITestInterface), original);
+
+                result.Should().Contain(@"void Method(ITestInterface instance)
+{
+    " + TestNativeAdapterName + @".Method1(""Callback_Method1"");
+    " + TestNativeAdapterName + @".Method2(""Callback_Method2"");
+}");
+            }
+        }
+
+        private static class TestNativeAdapter
+        {
+        }
+    }
+}
