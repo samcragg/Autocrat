593b4676e5637d121f5120d8581a1f65d3d2002c
Samuel Cragg <sam.cragg@outlook.com>
2019-07-27 07:00:10 +0100

    Add support for loading csproj files

---
+++ src/Autocrat.Compiler/ProjectLoader.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Buildalyzer;
+    using Buildalyzer.Workspaces;
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Loads the C# projects from disk.
+    /// </summary>
+    internal class ProjectLoader
+    {
+        private readonly AnalyzerManager analyzerManager = new AnalyzerManager();
+
+        /// <summary>
+        /// Gets the compilations for the specified project paths.
+        /// </summary>
+        /// <param name="paths">The paths of the project files.</param>
+        /// <returns>
+        /// A task that represents the compilations of the projects.
+        /// </returns>
+        public virtual Task<Compilation[]> GetCompilationsAsync(string[] paths)
+        {
+            var tasks = new List<Task<Compilation>>();
+            foreach (string path in paths)
+            {
+                IEnumerable<Project> projects = this.analyzerManager
+                    .GetProject(path)
+                    .GetWorkspace()
+                    .CurrentSolution
+                    .Projects;
+
+                tasks.AddRange(projects.Select(p => p.GetCompilationAsync()));
+            }
+
+            return Task.WhenAll(tasks.ToArray());
+        }
+    }
+}
---
+++ tests/Compiler.Tests/ProjectLoaderTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Compiler.Tests
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Microsoft.CodeAnalysis;
+    using Xunit;
+
+    [Trait("Category", "Integration")]
+    public class ProjectLoaderTests
+    {
+        private readonly ProjectLoader loader = new ProjectLoader();
+
+        public sealed class GetCompilationsAsyncTests : ProjectLoaderTests
+        {
+            [Fact]
+            public async Task ShouldLoadAllTheProjects()
+            {
+                using (var project1 = new TemporaryFile())
+                using (var project2 = new TemporaryFile())
+                {
+                    WriteProjectFile(project1.Filename);
+                    WriteProjectFile(project2.Filename);
+
+                    Compilation[] compilations = await this.loader.GetCompilationsAsync(new[]
+                    {
+                        project1.Filename,
+                        project2.Filename
+                    });
+
+                    compilations.Should().HaveCount(2);
+                }
+            }
+
+            private static void WriteProjectFile(string path)
+            {
+                const string Template = @"<Project Sdk='Microsoft.NET.Sdk'>
+<PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+  </PropertyGroup>
+</Project>";
+
+                File.WriteAllText(path, Template);
+            }
+        }
+
+        private sealed class TemporaryFile : IDisposable
+        {
+            public TemporaryFile()
+            {
+                // The file MUST end with "csproj" so the loader know what language
+                // the project is in
+                this.Filename = Path.Combine(
+                    Path.GetTempPath(),
+                    Guid.NewGuid() + ".csproj");
+            }
+
+            public string Filename { get; }
+
+            public void Dispose()
+            {
+                try
+                {
+                    File.Delete(this.Filename);
+                }
+                catch
+                {
+                }
+            }
+        }
+    }
+}
