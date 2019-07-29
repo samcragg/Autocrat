a218e8f41fec8755b84e9552cf60c566350f4ecc
Samuel Cragg <sam.cragg@outlook.com>
2019-07-29 06:56:00 +0100

    Allow the extraction of types from a symbol

---
+++ src/Autocrat.Compiler/NamedTypeVisitor.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Extracts the nested type symbols from a symbol.
+    /// </summary>
+    internal class NamedTypeVisitor : SymbolVisitor
+    {
+        private readonly HashSet<INamedTypeSymbol> types = new HashSet<INamedTypeSymbol>();
+
+        /// <summary>
+        /// Gets the visited symbols.
+        /// </summary>
+        public IReadOnlyCollection<INamedTypeSymbol> Types => this.types;
+
+        /// <inheritdoc />
+        public override void VisitNamedType(INamedTypeSymbol symbol)
+        {
+            this.types.Add(symbol);
+        }
+
+        /// <inheritdoc />
+        public override void VisitNamespace(INamespaceSymbol symbol)
+        {
+            foreach (INamespaceOrTypeSymbol childSymbol in symbol.GetMembers())
+            {
+                childSymbol.Accept(this);
+            }
+        }
+    }
+}
---
+++ tests/Compiler.Tests/NamedTypeVisitorTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Compiler.Tests
+{
+    using System.Linq;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Microsoft.CodeAnalysis;
+    using Xunit;
+
+    public class NamedTypeVisitorTests
+    {
+        private readonly NamedTypeVisitor visitor = new NamedTypeVisitor();
+
+        public sealed class VisitTests : NamedTypeVisitorTests
+        {
+            [Fact]
+            public void ShouldExtractDeclaredTypes()
+            {
+                Compilation compilation = CompilationHelper.CompileCode(@"
+namespace TestNamespace
+{
+    public class ExampleClass
+    {
+    }
+}");
+                this.visitor.Visit(GetNamespace(compilation, "TestNamespace"));
+
+                this.visitor.Types.Should().ContainSingle()
+                    .Which.Should().Be(compilation.GetTypeByMetadataName("TestNamespace.ExampleClass"));
+            }
+
+            [Fact]
+            public void ShouldExtractTypesInNestedNamespaces()
+            {
+                Compilation compilation = CompilationHelper.CompileCode(@"
+namespace OuterNamespace.InnerNamespace
+{
+    public class ExampleClass
+    {
+    }
+}");
+                this.visitor.Visit(GetNamespace(compilation, "OuterNamespace"));
+
+                this.visitor.Types.Should().ContainSingle()
+                    .Which.Should().Be(compilation.GetTypeByMetadataName("OuterNamespace.InnerNamespace.ExampleClass"));
+            }
+
+            private static INamespaceSymbol GetNamespace(Compilation compilation, string name)
+            {
+                return compilation.GlobalNamespace.GetNamespaceMembers()
+                    .Single(n => n.Name == name);
+            }
+        }
+    }
+}
