96b7a69837811e47225fd5ec948f62c8a0a18283
Samuel Cragg <sam.cragg@outlook.com>
2019-08-01 06:24:52 +0100

    Allow the generation of assemblies with initializers rewritten

---
+++ src/Autocrat.Compiler/CodeGenerator.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Autocrat.Abstractions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    /// <summary>
+    /// Creates the generated code for the managed and native parts of the
+    /// application.
+    /// </summary>
+    internal class CodeGenerator
+    {
+        private readonly List<SyntaxTree> generatedCode = new List<SyntaxTree>();
+
+        /// <summary>
+        /// Gets or sets the function to call to create a
+        /// <see cref="ServiceFactory"/>.
+        /// </summary>
+        internal static Func<Compilation, ServiceFactory> ServiceFactory { get; set; }
+            = c => new ServiceFactory(c);
+
+        /// <summary>
+        /// Generates the code for the specified compilation.
+        /// </summary>
+        /// <param name="destination">Where to save the code to.</param>
+        /// <param name="compilation">Contains the compiled information.</param>
+        public void Emit(Stream destination, Compilation compilation)
+        {
+            ServiceFactory factory = ServiceFactory(compilation);
+
+            this.generatedCode.AddRange(compilation.SyntaxTrees);
+            this.RewriteInitializers(factory, compilation);
+
+            this.Compile(compilation)
+                .Emit(destination);
+        }
+
+        private Compilation Compile(Compilation compilation)
+        {
+            var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+
+            return CSharpCompilation
+                .Create("TestAssembly", options: options)
+                .AddReferences(compilation.References)
+                .AddSyntaxTrees(this.generatedCode);
+        }
+
+        private void RewriteInitializers(ServiceFactory factory, Compilation compilation)
+        {
+            INamedTypeSymbol initializer = compilation.GetTypeByMetadataName(
+                "Autocrat.Abstractions." + nameof(IInitializer));
+
+            InterfaceResolver interfaceResolver = factory.GetInterfaceResolver();
+            InitializerGenerator generator = factory.CreateInitializerGenerator();
+            foreach (INamedTypeSymbol type in interfaceResolver.FindClasses(initializer))
+            {
+                generator.AddClass(type);
+            }
+
+            this.generatedCode.Add(
+                CSharpSyntaxTree.Create(generator.Generate()));
+        }
+    }
+}
---
+++ tests/Compiler.Tests/CodeGeneratorTests.cs
@@ -0,0 +1,75 @@
+﻿namespace Compiler.Tests
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using NSubstitute;
+    using Xunit;
+
+    public class CodeGeneratorTests : IDisposable
+    {
+        private readonly ServiceFactory factory;
+        private readonly CodeGenerator generator;
+
+        private CodeGeneratorTests()
+        {
+            this.factory = Substitute.For<ServiceFactory>(new object[] { null });
+            this.factory.CreateInitializerGenerator()
+                    .Generate().Returns(SyntaxFactory.ParseCompilationUnit("namespace X {}"));
+
+            CodeGenerator.ServiceFactory = _ => this.factory;
+            this.generator = new CodeGenerator();
+        }
+
+        public void Dispose()
+        {
+            CodeGenerator.ServiceFactory = null;
+        }
+
+        public sealed class EmitTests : CodeGeneratorTests
+        {
+            [Fact]
+            public void ShouldIncludeTheOriginalCode()
+            {
+                Assembly assembly = this.EmitCode(@"namespace Original
+{
+    public class OriginalClass
+    {
+    }
+}");
+
+                assembly.GetType("Original.OriginalClass").Should().NotBeNull();
+            }
+
+            [Fact]
+            public void ShouldRewriteTheInitializers()
+            {
+                CompilationUnitSyntax unit = SyntaxFactory.ParseCompilationUnit(@"namespace Initialization
+{
+    public class Test
+    {
+    }
+}");
+                this.factory.CreateInitializerGenerator()
+                    .Generate().Returns(unit);
+
+                Assembly assembly = this.EmitCode();
+
+                assembly.GetType("Initialization.Test").Should().NotBeNull();
+            }
+
+            private Assembly EmitCode(string originalCode = "namespace X {}")
+            {
+                using (var stream = new MemoryStream())
+                {
+                    this.generator.Emit(stream, CompilationHelper.CompileCode(originalCode));
+                    return Assembly.Load(stream.ToArray());
+                }
+            }
+        }
+    }
+}
