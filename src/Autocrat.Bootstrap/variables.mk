da575b4fb895b4668996f79d3686d6f29f57b436
Samuel Cragg <sam.cragg@outlook.com>
2019-11-06 22:28:38 +0000

    Add makefiles
    Add initial support for building and testing with GCC under Linux

---
+++ src/Autocrat.Bootstrap/variables.mk
@@ -0,0 +1,15 @@
+mode ?= Release
+OBJ_DIR = obj/$(mode)
+WARNINGFLAGS = -Werror -Wall -Wextra -Wpedantic
+
+CXXFLAGS = $(WARNINGFLAGS) -I ../../src/Autocrat.Bootstrap/include -I ../../libs -pthread -march=x86-64 -std=c++17
+ifeq ($(mode), Debug)
+	CXXFLAGS += -g
+else
+	CXXFLAGS += -O2
+endif
+
+define build_object
+	@mkdir -p $(dir $(2))
+	g++ $(1) -c -o $(2) $(CXXFLAGS)
+endef
