e8e69604debd17470902ed682fa787c03a703dc0
Samuel Cragg <sam.cragg@outlook.com>
2019-11-09 08:11:38 +0000

    Unify PAL signal handling tests

--- tests/Bootstrap.Tests/tests/PalServicesTests.cpp
+++ tests/Bootstrap.Tests/tests/PalServicesTests.cpp
@@ -1,25 +1,46 @@
-#define PAL_MOCK_H
+#undef UNIT_TESTS
 #include "pal.h"
-#include "pal_win32.h"
 
 #include <atomic>
 #include <chrono>
 #include <thread>
 #include <gtest/gtest.h>
 
+#if defined(__linux__)
+#include <signal.h>
+#endif
+
 using namespace std::chrono_literals;
 using std::chrono::system_clock;
 
-class PalWin32ServicesTests : public testing::Test
+class PalServicesTests : public testing::Test
 {
 protected:
 };
 
-using PalWin32ServicesDeathTest = PalWin32ServicesTests;
+using PalServicesDeathTest = PalServicesTests;
 
 namespace
 {
     std::atomic_bool is_running;
+
+    void enable_automatic_ctrl_c_handling(bool enabled)
+    {
+#if defined(_WIN32)
+        SetConsoleCtrlHandler(nullptr, !enabled);
+#else
+        ((void)enabled);
+#endif
+    }
+
+    void raise_ctrl_c()
+    {
+#if defined(_WIN32)
+        GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0);
+#else
+        raise(SIGINT);
+#endif
+    }
 
     void signal_handler()
     {
@@ -27,21 +48,22 @@ namespace
     }
 }
 
-TEST_F(PalWin32ServicesDeathTest, ShouldHandleConsoleCloseSignals)
+TEST_F(PalServicesDeathTest, ShouldHandleConsoleCloseSignals)
 {
     // Disable ctrl-c handling for our process (the child process inherits our
     // console and this setting, so when we fire a ctrl-c signal to the console
     // window we'd also receive it)
-    SetConsoleCtrlHandler(nullptr, TRUE);
+    enable_automatic_ctrl_c_handling(false);
+
     EXPECT_EXIT(
         {
             // Restore ctrl-c handling now we're in a separate process
-            SetConsoleCtrlHandler(nullptr, FALSE);
+            enable_automatic_ctrl_c_handling(true);
 
             is_running = true;
             pal::set_close_signal_handler(&signal_handler);
 
-            GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0);
+            raise_ctrl_c();
             auto end = system_clock::now() + 1s;
             while (is_running)
             {
@@ -57,5 +79,5 @@ TEST_F(PalWin32ServicesDeathTest, ShouldHandleConsoleCloseSignals)
         ::testing::ExitedWithCode(1),
         "");
 
-    SetConsoleCtrlHandler(nullptr, FALSE);
+    enable_automatic_ctrl_c_handling(false);
 }
