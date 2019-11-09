f073861c39322eb3699ff73189702186ccb3a8f7
Samuel Cragg <sam.cragg@outlook.com>
2019-11-09 08:32:23 +0000

    Unify PAL socket tests

--- tests/Bootstrap.Tests/tests/PalSocketTests.cpp
+++ tests/Bootstrap.Tests/tests/PalSocketTests.cpp
@@ -1,20 +1,35 @@
-#define PAL_MOCK_H
+#undef UNIT_TESTS
 #include "pal.h"
-#include "pal_win32.h"
 
 #include <string_view>
 #include <system_error>
-#include <ws2tcpip.h>
 #include <gtest/gtest.h>
 
-class PalWin32SocketTests : public testing::Test
+#if defined(_WIN32)
+#include <ws2tcpip.h>
+#else
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#endif
+
+namespace
+{
+#if defined(_WIN32)
+    using socklen_t = int;
+#else
+    using SOCKET = int;
+    constexpr int INVALID_SOCKET = -1;
+#endif
+}
+
+class PalSocketTests : public testing::Test
 {
 public:
 protected:
     std::uint16_t GetPort(const pal::socket_handle& handle)
     {
         sockaddr_in address = {};
-        int length = sizeof(address);
+        socklen_t length = sizeof(address);
         getsockname(handle.handle(), reinterpret_cast<sockaddr*>(&address), &length);
         return ntohs(address.sin_port);
     }
@@ -30,7 +45,7 @@ protected:
     }
 };
 
-TEST_F(PalWin32SocketTests, SocketAddressShouldHandleIP4Addresses)
+TEST_F(PalSocketTests, SocketAddressShouldHandleIP4Addresses)
 {
     const char ip[] = "1.2.3.4";
 
@@ -39,7 +54,7 @@ TEST_F(PalWin32SocketTests, SocketAddressShouldHandleIP4Addresses)
     EXPECT_STREQ(ip, address.to_string().c_str());
 }
 
-TEST_F(PalWin32SocketTests, SocketAddressShouldHandleIP6Addresses)
+TEST_F(PalSocketTests, SocketAddressShouldHandleIP6Addresses)
 {
     const char ip[] = "1:2:3:4:5:6:7:8";
 
@@ -48,7 +63,7 @@ TEST_F(PalWin32SocketTests, SocketAddressShouldHandleIP6Addresses)
     EXPECT_STREQ(ip, address.to_string().c_str());
 }
 
-TEST_F(PalWin32SocketTests, SocketAddressShouldGetAndSetThePort)
+TEST_F(PalSocketTests, SocketAddressShouldGetAndSetThePort)
 {
     const std::uint16_t port = 12345;
     pal::socket_address address;
@@ -58,12 +73,12 @@ TEST_F(PalWin32SocketTests, SocketAddressShouldGetAndSetThePort)
     EXPECT_EQ(port, address.port());
 }
 
-TEST_F(PalWin32SocketTests, SocketHandleConstructorShouldThrowForSocketErrors)
+TEST_F(PalSocketTests, SocketHandleConstructorShouldThrowForSocketErrors)
 {
     EXPECT_THROW(pal::socket_handle(-1, -1), std::system_error);
 }
 
-TEST_F(PalWin32SocketTests, SocketHandleMoveAssignmentShouldMoveTheResource)
+TEST_F(PalSocketTests, SocketHandleMoveAssignmentShouldMoveTheResource)
 {
     pal::socket_handle original = pal::create_udp_socket();
     SOCKET original_handle = original.handle();
@@ -74,7 +89,7 @@ TEST_F(PalWin32SocketTests, SocketHandleMoveAssignmentShouldMoveTheResource)
     EXPECT_EQ(INVALID_SOCKET, original.handle());
 }
 
-TEST_F(PalWin32SocketTests, SocketHandleMoveConstructorShouldMoveTheResource)
+TEST_F(PalSocketTests, SocketHandleMoveConstructorShouldMoveTheResource)
 {
     pal::socket_handle original = pal::create_udp_socket();
     SOCKET original_handle = original.handle();
@@ -85,7 +100,7 @@ TEST_F(PalWin32SocketTests, SocketHandleMoveConstructorShouldMoveTheResource)
     EXPECT_EQ(INVALID_SOCKET, original.handle());
 }
 
-TEST_F(PalWin32SocketTests, BindShouldThrowForErrors)
+TEST_F(PalSocketTests, BindShouldThrowForErrors)
 {
     pal::socket_handle closed = pal::create_udp_socket();
     closed.~socket_handle();
@@ -93,7 +108,7 @@ TEST_F(PalWin32SocketTests, BindShouldThrowForErrors)
     EXPECT_THROW(pal::bind(closed, pal::socket_address::any_ipv4()), std::system_error);
 }
 
-TEST_F(PalWin32SocketTests, PollShouldHandleEmptyLists)
+TEST_F(PalSocketTests, PollShouldHandleEmptyLists)
 {
     pal::socket_map<int> list;
     bool called = false;
@@ -103,7 +118,7 @@ TEST_F(PalWin32SocketTests, PollShouldHandleEmptyLists)
     EXPECT_FALSE(called);
 }
 
-TEST_F(PalWin32SocketTests, PollShouldInvokeTheCallbackIfDataIsAvailable)
+TEST_F(PalSocketTests, PollShouldInvokeTheCallbackIfDataIsAvailable)
 {
     pal::socket_handle server = pal::create_udp_socket();
     pal::bind(server, pal::socket_address::any_ipv4());
@@ -120,7 +135,8 @@ TEST_F(PalWin32SocketTests, PollShouldInvokeTheCallbackIfDataIsAvailable)
     EXPECT_EQ(123, called_value);
 }
 
-TEST_F(PalWin32SocketTests, PollShouldThrowForErrors)
+#if defined(_WIN32)
+TEST_F(PalSocketTests, PollShouldThrowForErrors)
 {
     pal::socket_handle closed = pal::create_udp_socket();
     closed.~socket_handle();
@@ -130,8 +146,9 @@ TEST_F(PalWin32SocketTests, PollShouldThrowForErrors)
 
     EXPECT_THROW(pal::poll(list, [](auto&, auto, auto) {}), std::system_error);
 }
+#endif
 
-TEST_F(PalWin32SocketTests, RecvFromShouldReturnZeroIfThereIsNoDataReady)
+TEST_F(PalSocketTests, RecvFromShouldReturnZeroIfThereIsNoDataReady)
 {
     pal::socket_handle socket = pal::create_udp_socket();
     pal::bind(socket, pal::socket_address::any_ipv4());
@@ -142,7 +159,7 @@ TEST_F(PalWin32SocketTests, RecvFromShouldReturnZeroIfThereIsNoDataReady)
     EXPECT_EQ(0, result);
 }
 
-TEST_F(PalWin32SocketTests, RecvFromShouldThrowForErrors)
+TEST_F(PalSocketTests, RecvFromShouldThrowForErrors)
 {
     pal::socket_handle closed = pal::create_udp_socket();
     closed.~socket_handle();
@@ -151,7 +168,7 @@ TEST_F(PalWin32SocketTests, RecvFromShouldThrowForErrors)
     EXPECT_THROW(pal::recv_from(closed, buffer, sizeof(buffer), nullptr), std::system_error);
 }
 
-TEST_F(PalWin32SocketTests, RecvFromShouldReturnTheSentData)
+TEST_F(PalSocketTests, RecvFromShouldReturnTheSentData)
 {
     pal::socket_handle server = pal::create_udp_socket();
     pal::bind(server, pal::socket_address::any_ipv4());
@@ -164,7 +181,7 @@ TEST_F(PalWin32SocketTests, RecvFromShouldReturnTheSentData)
     EXPECT_STREQ("1234", buffer);
 }
 
-TEST_F(PalWin32SocketTests, RecvFromShouldSetTheSender)
+TEST_F(PalSocketTests, RecvFromShouldSetTheSender)
 {
     pal::socket_handle server = pal::create_udp_socket();
     pal::bind(server, pal::socket_address::any_ipv4());
