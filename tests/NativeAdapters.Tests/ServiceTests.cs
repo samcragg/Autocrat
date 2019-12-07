2df20678a380e4a6a2321cf9653849ebec612b53
Samuel Cragg <sam.cragg@outlook.com>
2019-12-07 07:50:27 +0000

    Add unit tests
    Ensure the managed services implement the interface methods.

---
+++ tests/NativeAdapters.Tests/ServiceTests.cs
@@ -0,0 +1,56 @@
+﻿namespace NativeAdapters.Tests
+{
+    using System;
+    using System.Reflection;
+    using Autocrat.Abstractions;
+    using Autocrat.NativeAdapters;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ServiceTests
+    {
+        private static void AssertImplementsInterface<TInterface>(Type classType)
+        {
+            static Type GetParameterType(ParameterInfo pi)
+            {
+                if (pi.ParameterType.GetCustomAttribute<NativeDelegateAttribute>() != null)
+                {
+                    return typeof(int);
+                }
+                else
+                {
+                    return pi.ParameterType;
+                }
+            }
+
+            foreach (MethodInfo method in typeof(TInterface).GetMethods())
+            {
+                Type[] parameters = Array.ConvertAll(method.GetParameters(), GetParameterType);
+
+                MethodInfo classMethod = classType.GetMethod(method.Name, parameters);
+                
+                classMethod.Should().NotBeNull();
+                classMethod.IsStatic.Should().BeTrue();
+                classMethod.ReturnType.Should().Be(method.ReturnType);
+            }
+        }
+
+        public sealed class NetworkServiceTests : ServiceTests
+        {
+            [Fact]
+            public void ShouldImplementTheInterfaceMethods()
+            {
+                AssertImplementsInterface<INetworkService>(typeof(NetworkService));
+            }
+        }
+
+        public sealed class TimerServiceTests : ServiceTests
+        {
+            [Fact]
+            public void ShouldImplementTheInterfaceMethods()
+            {
+                AssertImplementsInterface<ITimerService>(typeof(TimerService));
+            }
+        }
+    }
+}
