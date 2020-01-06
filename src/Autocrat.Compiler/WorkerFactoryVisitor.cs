e97005e099b37c7f9e37a421bdac37ee29646add
Samuel Cragg <sam.cragg@outlook.com>
2020-01-06 08:23:02 +0000

    Find IWorkerFactory types
    Allow the discovery of invocations of `GetWorker<T>` so the constructors
    for the types can be generated and passed to the native methods.

---
+++ src/Autocrat.Compiler/WorkerFactoryVisitor.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using Autocrat.Abstractions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+    /// <summary>
+    /// Extracts the types created by the <see cref="IWorkerFactory"/> interface.
+    /// </summary>
+    internal class WorkerFactoryVisitor : CSharpSyntaxWalker
+    {
+        private readonly SemanticModel? model;
+        private readonly INamedTypeSymbol workerFactoryInterface;
+        private readonly HashSet<ITypeSymbol> workerTypes = new HashSet<ITypeSymbol>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerFactoryVisitor"/> class.
+        /// </summary>
+        /// <param name="compilation">The compilation to visit.</param>
+        public WorkerFactoryVisitor(Compilation compilation)
+        {
+            this.workerFactoryInterface = compilation.GetTypeByMetadataName(
+                "Autocrat.Abstractions." + nameof(IWorkerFactory));
+
+            foreach (SyntaxTree tree in compilation.SyntaxTrees)
+            {
+                this.model = compilation.GetSemanticModel(tree);
+                this.Visit(tree.GetRoot());
+            }
+        }
+
+        /// <summary>
+        /// Gets the generic arguments passed into <see cref="IWorkerFactory.GetWorker{T}"/>.
+        /// </summary>
+        public IReadOnlyCollection<ITypeSymbol> WorkerTypes => this.workerTypes;
+
+        /// <inheritdoc />
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            if (this.model != null)
+            {
+                SymbolInfo info = this.model.GetSymbolInfo(node.Expression);
+                if (info.Symbol?.Kind == SymbolKind.Method)
+                {
+                    var method = (IMethodSymbol)info.Symbol;
+                    if (string.Equals(method.Name, nameof(IWorkerFactory.GetWorker), StringComparison.Ordinal) &&
+                        SymbolEqualityComparer.Default.Equals(method.ContainingType, this.workerFactoryInterface))
+                    {
+                        this.workerTypes.Add(method.TypeArguments[0]);
+                    }
+                }
+            }
+
+            base.VisitInvocationExpression(node);
+        }
+    }
+}
---
+++ tests/Compiler.Tests/WorkerFactoryVisitorTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Compiler.Tests
+{
+    using System.Linq;
+    using Autocrat.Compiler;
+    using FluentAssertions;
+    using Microsoft.CodeAnalysis;
+    using Xunit;
+
+    public class WorkerFactoryVisitorTests
+    {
+        public sealed class WorkerTypes : WorkerFactoryVisitorTests
+        {
+            [Fact]
+            public void ShouldExtractUniqueTypes()
+            {
+                Compilation compilation = CompilationHelper.CompileCode(@"
+namespace Autocrat.Abstractions
+{
+    public interface IWorkerFactory
+    {
+        void GetWorker<T>();
+    }
+}
+
+namespace TestNamespace
+{
+    using Autocrat.Abstractions;
+
+    public class Worker1 { }
+
+    public class Worker2 { }
+
+    public class ExampleClass
+    {
+        public void ExampleMethod(IWorkerFactory factory)
+        {
+            factory.GetWorker<Worker1>();
+            factory.GetWorker<Worker2>();
+            factory.GetWorker<Worker1>();
+        }
+    }
+}");
+
+                var visitor = new WorkerFactoryVisitor(compilation);
+
+                visitor.WorkerTypes.Should().HaveCount(2);
+                visitor.WorkerTypes.Select(x => x.ToDisplayString())
+                       .Should().BeEquivalentTo("TestNamespace.Worker1", "TestNamespace.Worker2");
+            }
+        }
+    }
+}
