a97066e63c0e6535e80e0b65fc1787975a071254
Samuel Cragg <sam.cragg@outlook.com>
2020-02-28 06:45:40 +0000

    Add an example project

---
+++ Examples/TimerExample/Initializer.cs
@@ -0,0 +1,22 @@
+﻿namespace TimerExample
+{
+    using System;
+    using Autocrat.Abstractions;
+
+    public class Initializer : IInitializer
+    {
+        private readonly ITimerService timerService;
+
+        public Initializer(ITimerService timerService)
+        {
+            this.timerService = timerService;
+        }
+
+        public void OnConfigurationLoaded()
+        {
+            var handler = new TimerHandler(null);
+            this.timerService.RegisterRepeat(TimeSpan.FromSeconds(1), handler.OnTimer);
+            Console.WriteLine("Registered timer");
+        }
+    }
+}
---
+++ Examples/TimerExample/TimerHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace TimerExample
+{
+    using System;
+    using Autocrat.Abstractions;
+
+    internal sealed class TimerHandler
+    {
+        private readonly IWorkerFactory workerFactory;
+
+        public TimerHandler(IWorkerFactory workerFactory)
+        {
+            this.workerFactory = workerFactory;
+        }
+
+        public void OnTimer(int token)
+        {
+            TimeSpan now = DateTime.UtcNow.TimeOfDay;
+
+            // Temporary workaround until the GC is handled by the native code
+            GC.TryStartNoGCRegion(1024 * 1024);
+
+            TimerState state = this.workerFactory.GetWorker<TimerState>();
+            state.InvocationCount++;
+
+            Console.WriteLine("{0}: {1}", now, state.InvocationCount);
+
+            GC.EndNoGCRegion();
+        }
+
+        public class TimerState
+        {
+            public TimerState()
+            {
+                Console.WriteLine("TimerState constructor");
+            }
+
+            public int InvocationCount { get; set; }
+        }
+    }
+}
