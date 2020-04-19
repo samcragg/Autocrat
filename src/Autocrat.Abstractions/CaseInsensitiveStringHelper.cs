e311840ca83e50217274c7b986779822f15d805c
Samuel Cragg <sam.cragg@outlook.com>
2020-04-19 08:48:12 +0100

    Add string helper

---
+++ src/Autocrat.Abstractions/CaseInsensitiveStringHelper.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Samuel Cragg.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license information.
+
+namespace Autocrat.Abstractions
+{
+    using System;
+    using System.ComponentModel;
+    using static System.Diagnostics.Debug;
+
+    /// <summary>
+    /// Contains methods for comparing strings in a case-insensitive way.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Internal class used by the compiler and runtime to generate classes to
+    /// deserialize configuration properties.
+    /// </para><para>
+    /// This class only takes into account uppercase ASCII letters (i.e. doesn't
+    /// follow the Unicode definition of uppercase/lowercase).
+    /// </para>
+    /// </remarks>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class CaseInsensitiveStringHelper
+    {
+        /// <summary>
+        /// Indicates whether two strings are equal.
+        /// </summary>
+        /// <param name="value">An uppercase string to compare to.</param>
+        /// <param name="other">
+        /// Contains the value to compare to the string.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the buffer equals the specified string; otherwise
+        /// <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// The string passing in as <c>value</c> MUST already be uppercase.
+        /// </remarks>
+        public static bool Equals(string value, ReadOnlySpan<byte> other)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            Assert(value.ToUpperInvariant() == value, "value must be uppercase");
+            if (value.Length != other.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                // Make uppercase
+                uint c = other[i];
+                if ((c - 'a') <= ('z' - 'a'))
+                {
+                    c -= 'a' - 'A';
+                }
+
+                if (c != value[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the hash code for the specified string.
+        /// </summary>
+        /// <param name="value">The string.</param>
+        /// <returns>A 32-bit signed integer calculated from the string.</returns>
+        public static int GetHashCode(ReadOnlySpan<byte> value)
+        {
+            // Fowler–Noll–Vo hash function
+            // https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
+            const uint FnvOffsetBasis = 2166136261;
+            const uint FnvPrime = 16777619;
+
+            uint hash = FnvOffsetBasis;
+            for (int i = 0; i < value.Length; i++)
+            {
+                // Make uppercase
+                uint c = value[i];
+                if ((c - 'a') <= ('z' - 'a'))
+                {
+                    c -= 'a' - 'A';
+                }
+
+                hash = (hash ^ c) * FnvPrime;
+            }
+
+            return (int)hash;
+        }
+    }
+}
---
+++ tests/Abstractions.Tests/CaseInsensitiveStringHelperTests.cs
@@ -0,0 +1,68 @@
+﻿namespace Abstractions.Tests
+{
+    using System;
+    using System.Text;
+    using Autocrat.Abstractions;
+    using FluentAssertions;
+    using Xunit;
+
+    public class CaseInsensitiveStringHelperTests
+    {
+        private static ReadOnlySpan<byte> GetBytes(string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+
+        public sealed class EqualTests : CaseInsensitiveStringHelperTests
+        {
+            [Fact]
+            public void ShouldReturnFalseIfTwoStringsAreNotEqual()
+            {
+                bool result = CaseInsensitiveStringHelper.Equals("ONE", GetBytes("TWO"));
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnFalseIfTwoStringsHaveADifferentLength()
+            {
+                bool result = CaseInsensitiveStringHelper.Equals("FIRST", GetBytes("SECOND"));
+
+                result.Should().BeFalse();
+            }
+
+            [Fact]
+            public void ShouldReturnTrueIfTwoStringsAreEqualButDifferByCase()
+            {
+                bool result = CaseInsensitiveStringHelper.Equals("VALUE", GetBytes("value"));
+
+                result.Should().BeTrue();
+            }
+        }
+
+        public sealed class GetHashCodeTests : CaseInsensitiveStringHelperTests
+        {
+            [Fact]
+            public void ShouldReturnDifferentHashCodesForDifferentStrings()
+            {
+                // GetHashCode could return the same hash code for all values
+                // and still be valid, though not useful. The way the algorithm
+                // is implemented we know for values of the same length it will
+                // produce different results
+                int first = CaseInsensitiveStringHelper.GetHashCode(GetBytes("one"));
+                int second = CaseInsensitiveStringHelper.GetHashCode(GetBytes("two"));
+
+                first.Should().NotBe(second);
+            }
+
+            [Fact]
+            public void ShouldReturnTheSameHashCodeForStringsThatHaveDifferentCasing()
+            {
+                int lowercase = CaseInsensitiveStringHelper.GetHashCode(GetBytes("value"));
+                int uppercase = CaseInsensitiveStringHelper.GetHashCode(GetBytes("VALUE"));
+
+                lowercase.Should().Be(uppercase);
+            }
+        }
+    }
+}
